bb8e98275499416f0341a51eea996ff8eb3ea1b8
harshal ingole <harshal.ingole21@gmail.com>
2025-01-06 18:51:53 +0530

    chore: express,ts backend setup and configuration

---
+++ apps/backend/src/app.ts
@@ -0,0 +1,30 @@
+import express, { Application } from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+import errorHandler from 'errorhandler'
+import cookieParser from 'cookie-parser'
+
+dotenv.config()
+
+const app:Application = express()
+
+//Express Configuration 
+app.set("port",process.env.PORT)
+
+
+// Middleware
+app.use(cors({origin: process.env.CORS_ORIGIN}))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+
+
+// Api Routes
+
+app.use("/", (req,res,next) => {
+  res.send("Crypto Arbitrage Predictor")
+})
+
+
+
+export default app
